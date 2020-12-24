0cff61f7b496c16cbaf2c5df508db122f925bc2f
Andhar7 <70223638+Andhar7@users.noreply.github.com>
2020-12-24 20:14:04 +0100

    Initial Commit

---
+++ NasaAPI/NasaAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NasaAPIApp.swift
+//  NasaAPI
+//
+//  Created by Andrej Kling on 24.12.20.
+//
+
+import SwiftUI
+
+@main
+struct NasaAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
